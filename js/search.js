66db28180e84482aa45bf0ffb59014cdf5fe98d9
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-07-12 21:48:54 +0700

    Update

--- js/search.js
+++ js/search.js
@@ -18,7 +18,7 @@ function handleSearch() {
   });
   console.log(filterProduct);
   localStorage.setItem("myProductSearch", JSON.stringify(filterProduct));
-  window.location.href = `search-page.html?search=${inputSearch}`;
+  window.location.href = `http://127.0.0.1:5501/search-page.html?search=${inputSearch}`;
 }
 
 // Function renderSearchPage
