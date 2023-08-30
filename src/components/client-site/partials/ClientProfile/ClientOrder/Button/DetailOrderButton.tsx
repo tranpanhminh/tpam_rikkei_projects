1e51ff71dbb4986ea9c12df8e0ce4aec9db5fa99
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-08-30 10:56:07 +0700

    Update

--- src/components/client-site/partials/ClientProfile/ClientOrder/Button/DetailOrderButton.tsx
+++ src/components/client-site/partials/ClientProfile/ClientOrder/Button/DetailOrderButton.tsx
@@ -26,7 +26,6 @@ const DetailOrderButton: React.FC<DetailOrderProps> = ({ orderId }) => {
 
   const [user, setUser] = useState<any>(null);
   const [orderDatabase, setOrderDatabase] = useState<any>([]);
-  const [orderItem, setOrderItem] = useState<any>([]);
   const fetchUser = () => {
     axios
       .get(`http://localhost:7373/accounts/${getLoginData.loginId}`)
@@ -49,12 +48,13 @@ const DetailOrderButton: React.FC<DetailOrderProps> = ({ orderId }) => {
   let findOrder = orderDatabase.find((item: any) => {
     return item.orderId === orderId;
   });
-  setOrderItem(findOrder);
+
+  console.log("Find Order", findOrder);
 
   return (
     <>
       <Button type="primary" onClick={showModal}>
-        Open Modal
+        Detail
       </Button>
       <Modal
         title="Basic Modal"
@@ -66,15 +66,15 @@ const DetailOrderButton: React.FC<DetailOrderProps> = ({ orderId }) => {
         <div className={styles["list-input-my-profile"]}>
           <div className={styles["my-profile-input-item"]}>
             <p>Phone</p>
-            <input type="text" disabled value={orderItem.phone} />
+            <input type="text" disabled value={findOrder?.phone} />
           </div>
           <div className={styles["my-profile-input-item"]}>
             <p>Address</p>
-            <input type="text" disabled value={orderItem.address} />
+            <input type="text" disabled value={findOrder?.address} />
           </div>
           <div className={styles["my-profile-input-item"]}>
             <p>Status</p>
-            <input type="text" disabled value={orderItem.status} />
+            <input type="text" disabled value={findOrder?.status} />
           </div>
           <div className={styles["my-profile-input-item"]}>
             <p>Request Cancel</p>
@@ -112,27 +112,28 @@ const DetailOrderButton: React.FC<DetailOrderProps> = ({ orderId }) => {
           </thead>
 
           <tbody>
-            {orderItem.map((item: any) => {
-              return (
-                <tr>
-                  <td>{item.productId}</td>
-                  <td>
-                    <img src={item.productImage} alt="" />
-                  </td>
-                  <td>{item.productName}</td>
-                  <td>{item.productQuantity}</td>
-                  <td>{Number(item.price)}</td>
-                  <td>
-                    ${(item.productQuantity * item.price).toLocaleString()}
-                  </td>
-                </tr>
-              );
-            })}
+            {findOrder &&
+              findOrder.orderProduct.map((item: any) => {
+                return (
+                  <tr>
+                    <td>{item.productId}</td>
+                    <td>
+                      <img src={item.productImage} alt="" />
+                    </td>
+                    <td>{item.productName}</td>
+                    <td>{item.productQuantity}</td>
+                    <td>{Number(item.price)}</td>
+                    <td>
+                      ${(item.productQuantity * item.price).toLocaleString()}
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
         <div className={styles["my-profile-my-order-card"]}>
           <span className={styles["my-order-card-item"]}>
-            Item: {orderItem.length}
+            Item: {findOrder?.length}
           </span>
           <span className={styles["my-order-card-total-quantity"]}>
             Total: $270
