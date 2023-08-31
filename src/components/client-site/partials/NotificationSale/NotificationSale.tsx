41ea14c2ba2e2b78ffec0f4aa24008edf39af651
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-08-31 09:41:06 +0700

    Update

--- src/components/client-site/partials/NotificationSale/NotificationSale.tsx
+++ src/components/client-site/partials/NotificationSale/NotificationSale.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import {
+  RadiusBottomleftOutlined,
+  RadiusBottomrightOutlined,
+  RadiusUpleftOutlined,
+  RadiusUprightOutlined,
+} from "@ant-design/icons";
+import React, { useEffect, useMemo, useState } from "react";
+import { Button, Divider, Space, notification } from "antd";
+import type { NotificationPlacement } from "antd/es/notification/interface";
+import axios from "axios";
 
-function NotificationSale() {
-  return <></>;
-}
+const Context = React.createContext({ name: "Default" });
+
+const NotificationSale: React.FC = () => {
+  const [listOrders, setListOrders] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchOrders = () => {
+      axios
+        .get(`http://localhost:7373/orders/`)
+        .then((response) => setListOrders(response.data));
+    };
+  }, []);
+
+  const [api, contextHolder] = notification.useNotification();
+
+  const openNotification = (placement: NotificationPlacement) => {
+    api.info({
+      message: `Notification ${placement}`,
+      description: (
+        <Context.Consumer>{({ name }) => `Hello, ${name}!`}</Context.Consumer>
+      ),
+      placement,
+    });
+  };
+
+  return (
+    <>
+      <Button
+        type="primary"
+        onClick={() => openNotification("bottomLeft")}
+        icon={<RadiusBottomleftOutlined />}
+      >
+        bottomLeft
+      </Button>
+    </>
+  );
+};
 
 export default NotificationSale;
