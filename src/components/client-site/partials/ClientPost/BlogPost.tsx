00fa92255a29c93bae1a3bf991fbd61ed63d8d7b
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-09-02 23:41:49 +0700

    Update

--- src/components/client-site/partials/ClientPost/BlogPost.tsx
+++ src/components/client-site/partials/ClientPost/BlogPost.tsx
@@ -1,11 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../ClientPost/BlogPost.module.css";
 import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import { Pagination } from "antd";
+import axios from "axios";
 
 function BlogPost() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [listPosts, setListPosts] = useState<any>([]);
+  const [listProducts, setListProducts] = useState<any>([]);
+  const [listServices, setListServices] = useState<any>([]);
+
+  const fetchProducts = () => {
+    axios
+      .get(`http://localhost:7373/products`)
+      .then((response) => {
+        setListProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const fetchPosts = () => {
+    axios
+      .get(`http://localhost:7373/posts`)
+      .then((response) => {
+        setListPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const fetchServices = () => {
+    axios
+      .get(`http://localhost:7373/products`)
+      .then((response) => {
+        setListServices(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    fetchProducts();
+    fetchServices();
+  }, []);
+
   const navigate = useNavigate();
   const handleSearch = () => {
     navigate(`/search/${searchTerm}`);
@@ -14,130 +58,34 @@ function BlogPost() {
     <>
       <div className={styles["wrap-blog"]}>
         <div className={styles["list-blogs"]}>
-          <div className={styles["post-item"]}>
-            <div className={styles["post-thumbnail-item"]}>
-              <img
-                src="https://g.foolcdn.com/editorial/images/651176/shiba-inu-dogecoin-cryptocurrency-blockchain-network-getty.jpg"
-                alt=""
-                className={styles["img-thumbnail-item"]}
-              />
-            </div>
+          {listPosts &&
+            listPosts.map((post: any) => {
+              return (
+                <>
+                  <div className={styles["post-item"]}>
+                    <div className={styles["post-thumbnail-item"]}>
+                      <img
+                        src={post.image_url}
+                        alt=""
+                        className={styles["img-thumbnail-item"]}
+                      />
+                    </div>
 
-            <div className={styles["post-item-content"]}>
-              <h2 className={styles["post-item-title"]}>
-                Shiba Inu: Shytoshi Publishes 2nd Shibarium Blog
-              </h2>
-              <span className={styles["post-item-description"]}>
-                The blog talks about The Breeds concept which is on the
-                frontlines explaining to investors about the SHIB ecosystem’s
-                developments. Explaining to investors about the SHIB ecosystem’s
-                developments.
-              </span>
-              <div>
-                <Button variant="primary">Read More</Button>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["post-item"]}>
-            <div className={styles["post-thumbnail-item"]}>
-              <img
-                src="https://g.foolcdn.com/editorial/images/651176/shiba-inu-dogecoin-cryptocurrency-blockchain-network-getty.jpg"
-                alt=""
-                className={styles["img-thumbnail-item"]}
-              />
-            </div>
-
-            <div className={styles["post-item-content"]}>
-              <h2 className={styles["post-item-title"]}>
-                Shiba Inu: Shytoshi Publishes 2nd Shibarium Blog
-              </h2>
-              <span className={styles["post-item-description"]}>
-                The blog talks about The Breeds concept which is on the
-                frontlines explaining to investors about the SHIB ecosystem’s
-                developments. Explaining to investors about the SHIB ecosystem’s
-                developments.
-              </span>
-              <div>
-                <Button variant="primary">Read More</Button>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["post-item"]}>
-            <div className={styles["post-thumbnail-item"]}>
-              <img
-                src="https://g.foolcdn.com/editorial/images/651176/shiba-inu-dogecoin-cryptocurrency-blockchain-network-getty.jpg"
-                alt=""
-                className={styles["img-thumbnail-item"]}
-              />
-            </div>
-
-            <div className={styles["post-item-content"]}>
-              <h2 className={styles["post-item-title"]}>
-                Shiba Inu: Shytoshi Publishes 2nd Shibarium Blog
-              </h2>
-              <span className={styles["post-item-description"]}>
-                The blog talks about The Breeds concept which is on the
-                frontlines explaining to investors about the SHIB ecosystem’s
-                developments. Explaining to investors about the SHIB ecosystem’s
-                developments.
-              </span>
-              <div>
-                <Button variant="primary">Read More</Button>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["post-item"]}>
-            <div className={styles["post-thumbnail-item"]}>
-              <img
-                src="https://g.foolcdn.com/editorial/images/651176/shiba-inu-dogecoin-cryptocurrency-blockchain-network-getty.jpg"
-                alt=""
-                className={styles["img-thumbnail-item"]}
-              />
-            </div>
-
-            <div className={styles["post-item-content"]}>
-              <h2 className={styles["post-item-title"]}>
-                Shiba Inu: Shytoshi Publishes 2nd Shibarium Blog
-              </h2>
-              <span className={styles["post-item-description"]}>
-                The blog talks about The Breeds concept which is on the
-                frontlines explaining to investors about the SHIB ecosystem’s
-                developments. Explaining to investors about the SHIB ecosystem’s
-                developments.
-              </span>
-              <div>
-                <Button variant="primary">Read More</Button>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["post-item"]}>
-            <div className={styles["post-thumbnail-item"]}>
-              <img
-                src="https://g.foolcdn.com/editorial/images/651176/shiba-inu-dogecoin-cryptocurrency-blockchain-network-getty.jpg"
-                alt=""
-                className={styles["img-thumbnail-item"]}
-              />
-            </div>
-
-            <div className={styles["post-item-content"]}>
-              <h2 className={styles["post-item-title"]}>
-                Shiba Inu: Shytoshi Publishes 2nd Shibarium Blog
-              </h2>
-              <span className={styles["post-item-description"]}>
-                The blog talks about The Breeds concept which is on the
-                frontlines explaining to investors about the SHIB ecosystem’s
-                developments. Explaining to investors about the SHIB ecosystem’s
-                developments.
-              </span>
-              <div>
-                <Button variant="primary">Read More</Button>
-              </div>
-            </div>
-          </div>
+                    <div className={styles["post-item-content"]}>
+                      <h2 className={styles["post-item-title"]}>
+                        {post.post_title}
+                      </h2>
+                      <span className={styles["post-item-description"]}>
+                        {Array.from(post.post_content).slice(0, 200).join("")}
+                      </span>
+                      <div>
+                        <Button variant="primary">Read More</Button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
 
           <div className={styles["blogs-post-pagination"]}>
             <Pagination defaultCurrent={6} total={500} />
@@ -161,6 +109,10 @@ function BlogPost() {
                 Search
               </Button>
             </Form>
+          </div>
+
+          <div>
+            <p>Products</p>
           </div>
         </div>
       </div>
