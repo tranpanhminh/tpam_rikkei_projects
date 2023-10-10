1bce1dda6c350cd7e1a18f2819ab06d867964d7e
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-10-10 17:31:44 +0700

    Update

--- backend/src/models/orderItems.model.js
+++ backend/src/models/orderItems.model.js
@@ -21,7 +21,7 @@ const orderItemsModel = sequelize.define(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
     quantity: {
       type: DataTypes.INTEGER,
