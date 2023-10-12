cbf3fa0b63c93cf0fd36c443cbbb61711a1247f6
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-10-12 11:53:12 +0700

    Update

--- frontend/src/components/client-site/partials/ClientSpecialPage/SpecialPage.tsx
+++ frontend/src/components/client-site/partials/ClientSpecialPage/SpecialPage.tsx
@@ -3,13 +3,18 @@ import styles from "../ClientSpecialPage/SpecialPage.module.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 // import "../../../../assets/bootstrap-5.3.0-dist/css/bootstrap.min.css";
+// Import API
+// 1, Posts API
+const pagesAPI = process.env.REACT_APP_API_PAGES;
+
+// --------------------------------------------------
 
 function SpecialPage() {
   const { pageName } = useParams();
   const [pages, setPages] = useState<any>([]);
   const fetchPages = () => {
     axios
-      .get(`http://localhost:7373/pages/`)
+      .get(`${pagesAPI}`)
       .then((response) => {
         setPages(response.data);
       })
@@ -23,21 +28,21 @@ function SpecialPage() {
   }, [pageName]);
 
   let pageInfo = pages?.find((item: any) => {
-    return item.page_title.toLowerCase().replace(/ /g, "-") === pageName;
+    return item.title.toLowerCase().replace(/ /g, "-") === pageName;
   });
 
   document.title = `${
-    pageInfo ? `${pageInfo?.page_title} | PetShop` : "Loading..."
+    pageInfo ? `${pageInfo?.title} | PetShop` : "Loading..."
   }`;
 
   return (
     <>
       <div className={styles["contact-us-banner"]}>
-        <h1>{pageInfo?.page_title}</h1>
+        <h1>{pageInfo?.title}</h1>
       </div>
       <div className={styles["main-page-content"]}>
         {React.createElement("div", {
-          dangerouslySetInnerHTML: { __html: pageInfo?.page_content },
+          dangerouslySetInnerHTML: { __html: pageInfo?.content },
         })}
       </div>
     </>
