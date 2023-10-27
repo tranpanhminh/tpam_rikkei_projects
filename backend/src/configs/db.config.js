ae1044c1d0637470a101e7bdcdfc8cc01866c1c6
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-10-27 11:32:00 +0700

    Update

--- backend/src/configs/db.config.js
+++ backend/src/configs/db.config.js
@@ -1,10 +1,28 @@
+require("env").config();
+const databaseName = process.env.DB_NAME;
+const databaseUserName = process.env.DB_USERNAME;
+const databasePassword = process.env.DB_PASSWORD;
+const databaseHost = process.env.DB_HOST;
+
+// const sequelize = require("sequelize");
+// const connectMySQL = new sequelize(
+//   "project_module_3", // Tên Database
+//   "tranpanhminh", // Tên username
+//   "1234567890", // Tên Password
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//   }
+// );
+// module.exports = connectMySQL;
+
 const sequelize = require("sequelize");
 const connectMySQL = new sequelize(
-  "project_module_3", // Tên Database
-  "tranpanhminh", // Tên username
-  "1234567890", // Tên Password
+  databaseName, // Tên Database
+  databaseUserName, // Tên username
+  databasePassword, // Tên Password
   {
-    host: "localhost",
+    host: databaseHost,
     dialect: "mysql",
   }
 );
