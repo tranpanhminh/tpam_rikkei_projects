fe937b337a694766297082ecbf358bed91885bff
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-11-01 11:41:02 +0700

    Paypal Checkout Done

--- backend/src/modules/paypal/paypal.controller.ts
+++ backend/src/modules/paypal/paypal.controller.ts
@@ -20,82 +20,88 @@ export class PaypalController {
     @Req() req: any,
     @Res() res: any,
   ) {
-    try {
-      paymentData = {
-        intent: 'CAPTURE',
-        purchase_units: [
-          {
-            amount: {
-              currency_code: 'USD',
-              value: '5.00',
-            },
+    // Data này chỉ Test
+    paymentData = {
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: '5.00',
           },
-        ],
-        application_context: {
-          brand_name: 'mycompany.com',
-          landing_page: 'NO_PREFERENCE',
-          user_action: 'PAY_NOW',
-          return_url: `${BACKEND_PATH}/${path}/paypal/capture-order`,
-          cancel_url: `${BACKEND_PATH}/${path}/paypal/cancel-order`,
         },
-      };
+      ],
+      application_context: {
+        brand_name: 'mycompany.com',
+        landing_page: 'NO_PREFERENCE',
+        user_action: 'PAY_NOW',
+        return_url: `${BACKEND_PATH}/${path}/paypal/capture-order`,
+        cancel_url: `${BACKEND_PATH}/${path}/paypal/cancel-order`,
+      },
+    };
 
-      const params = new URLSearchParams();
-      params.append('grant_type', 'client_credentials');
-      console.log(params);
-      const {
-        data: { access_token },
-      } = await axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-          username: PAYPAL_CLIENT_ID,
-          password: PAYPAL_SECRET_KEY,
-        },
-      });
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
 
-      await axios
-        .post(`${PAYPAL_API}/v2/checkout/orders`, paymentData, {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        })
-        .then((response) => {
-          return res.json(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send('ERROR');
-    }
+    // const {
+    //   data: { access_token },
+    // } = await axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
+    //   headers: {
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   auth: {
+    //     username: PAYPAL_CLIENT_ID,
+    //     password: PAYPAL_SECRET_KEY,
+    //   },
+    // });
+
+    // await axios
+    //   .post(`${PAYPAL_API}/v2/checkout/orders`, paymentData, {
+    //     headers: {
+    //       Authorization: `Bearer ${access_token}`,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     return res.json(response.data);
+    //   })
+    //   .catch((error) => {
+    //     return res.json(error);
+    //   });
+
+    const result = await this.paypalService.createOrder(
+      paymentData,
+      params,
+      req,
+      res,
+    );
+    return result;
   }
 
   @Get('/capture-order')
   async captureOrder(@Req() req: any, @Res() res: any) {
     const { token } = req.query;
-    console.log(token);
 
-    await axios
-      .post(
-        `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
-        {},
-        {
-          auth: {
-            username: PAYPAL_CLIENT_ID,
-            password: PAYPAL_SECRET_KEY,
-          },
-        },
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.send(response.data);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+    // await axios
+    //   .post(
+    //     `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+    //     {},
+    //     {
+    //       auth: {
+    //         username: PAYPAL_CLIENT_ID,
+    //         password: PAYPAL_SECRET_KEY,
+    //       },
+    //     },
+    //   )
+    //   .then((response) => {
+    //     console.log(response.data);
+    //     res.send(response.data);
+    //   })
+    //   .catch((error) => {
+    //     res.send(error);
+    //   });
+
+    const result = await this.paypalService.captureOrder(token, req, res);
+    return result;
   }
 
   @Get('/cancel-order')
--- backend/src/modules/paypal/paypal.service.ts
+++ backend/src/modules/paypal/paypal.service.ts
@@ -1,33 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import * as paypal from 'paypal-rest-sdk';
+
+const path = process.env.SERVER_PATH;
+const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID;
+const PAYPAL_SECRET_KEY = process.env.PAYPAL_SECRET_KEY;
+const PAYPAL_API = process.env.PAYPAL_API;
+const BACKEND_PATH = process.env.BACKEND_PATH;
+const FRONTEND_PATH = process.env.FRONTEND_PATH;
+
+// -------------------------------------------------------
 
 @Injectable()
 export class PaypalService {
-  async createOrder(paymentData: any, request, response) {
-    // Thực hiện tạo thanh toán
-    paypal.payment.create(paymentData, function (error, payment) {
-      if (error) {
-        throw error;
-      } else {
-        console.log(payment);
-        for (let i = 0; i < payment.links.length; i++) {
-          if (payment.links[i].rel == 'approval_url') {
-            response.redirect(payment.links[i].href);
-          }
-        }
-      }
+  async createOrder(paymentData, params, req, res) {
+    const {
+      data: { access_token },
+    } = await axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      auth: {
+        username: PAYPAL_CLIENT_ID,
+        password: PAYPAL_SECRET_KEY,
+      },
     });
+    await axios
+      .post(`${PAYPAL_API}/v2/checkout/orders`, paymentData, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+      .then((response) => {
+        return res.json(response.data);
+      })
+      .catch((error) => {
+        return res.json(error);
+      });
   }
 
-  async captureOrder(payerId, paymentId, paymentData: any, request, response) {
-    paypal.payment.execute(paymentId, paymentData, function (error, payment) {
-      if (error) {
-        console.log(error, 'ERROR Excute');
-        throw error;
-      } else {
-        console.log(JSON.stringify(payment));
-        response.send('Success');
-      }
-    });
+  async captureOrder(token, req, res) {
+    await axios
+      .post(
+        `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+        {},
+        {
+          auth: {
+            username: PAYPAL_CLIENT_ID,
+            password: PAYPAL_SECRET_KEY,
+          },
+        },
+      )
+      .then((response) => {
+        return res.send(response.data);
+      })
+      .catch((error) => {
+        return res.send(error);
+      });
   }
 }
