2b58fa855ee2973e10ea6cb48aba80e7a4aebb70
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-11-08 11:59:42 +0700

    Update

--- frontend/src/components/client-site/partials/LoginSignUp/ResetPasswordForm.tsx
+++ frontend/src/components/client-site/partials/LoginSignUp/ResetPasswordForm.tsx
@@ -7,12 +7,14 @@ import { userRegister } from "../../../../api/users.api";
 // ----------------------------------------------------------
 function ResetPasswordForm() {
   const navigate = useNavigate();
+  const [show, setShow] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [searchParams] = useSearchParams();
+  const getToken = searchParams.get("resetToken");
 
   const handleRequestResetPassword = async () => {
-    const 
+    // const;
     // const result = await userRegister(userInfo);
     // if (result) {
     //   navigate("/login");
@@ -23,53 +25,56 @@ function ResetPasswordForm() {
 
   return (
     <div className={styles["outside-form-login"]}>
-      <div className={styles["form-signup"]}>
-        <h3 className={styles["signup-title"]}>Request Reset Password</h3>
-        <input
-          type="email"
-          placeholder="Email"
-          id={styles["input-email"]}
-          required
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <button
-          className={styles["signup-button"]}
-          onClick={handleRequestResetPassword}
-        >
-          Request Reset Password
-        </button>
-        <p className={styles["login-sentence"]}>
-          Already have an account?{" "}
-          <NavLink to="/login" className={styles["login-text"]}>
-            Login
-          </NavLink>
-        </p>
-      </div>
-
-      <div className={styles["form-signup"]}>
-        <h3 className={styles["signup-title"]}>Reset Password</h3>
-        <input
-          type="email"
-          placeholder="Email"
-          id={styles["input-password"]}
-          required
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button
-          className={styles["signup-button"]}
-          onClick={handleResetPassword}
-        >
-          Request Reset Password
-        </button>
-        <p className={styles["login-sentence"]}>
-          Already have an account?{" "}
-          <NavLink to="/login" className={styles["login-text"]}>
-            Login
-          </NavLink>
-        </p>
-      </div>
+      {getToken ? (
+        <div className={styles["form-signup"]}>
+          <h3 className={styles["signup-title"]}>Reset Password</h3>
+          <input
+            type="text"
+            placeholder="New Password"
+            id={styles["input-password"]}
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button
+            className={styles["signup-button"]}
+            onClick={handleResetPassword}
+          >
+            Reset Password
+          </button>
+          <p className={styles["login-sentence"]}>
+            Already have an account?
+            <NavLink to="/login" className={styles["login-text"]}>
+              Login
+            </NavLink>
+          </p>
+        </div>
+      ) : (
+        <div className={styles["form-signup"]}>
+          <h3 className={styles["signup-title"]}>Request Reset Password</h3>
+          <input
+            type="email"
+            placeholder="Email"
+            id={styles["input-email"]}
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <button
+            className={styles["signup-button"]}
+            onClick={handleRequestResetPassword}
+          >
+            Request Reset Password
+          </button>
+          <span>EEEEE</span>
+          <p className={styles["login-sentence"]}>
+            Already have an account?{" "}
+            <NavLink to="/login" className={styles["login-text"]}>
+              Login
+            </NavLink>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
