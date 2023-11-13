d752ff8b06ec803e9c100bda26228e7157c2e8ec
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-11-13 22:53:19 +0700

    Update

--- backend/src/modules/users/dto/resetPassword.dto.ts
+++ backend/src/modules/users/dto/resetPassword.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 export class ResetPasswordDTO {
   @IsNotEmpty({ message: 'New Password should not be empty' })
   @IsString({ message: 'New Password should be string' })
   @MinLength(8, { message: 'New Password must be at least 8 characters' })
+  @MaxLength(20, { message: 'Password must be < 20 characters' })
   password?: string;
 }
