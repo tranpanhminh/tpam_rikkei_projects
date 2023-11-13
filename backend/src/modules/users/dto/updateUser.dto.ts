2ffe16fa88abd49de41ee7323cab0e1b728c7e92
tranpanhminh <tranphuocanhminh73@gmail.com>
2023-11-13 23:07:31 +0700

    Update

--- backend/src/modules/users/dto/updateUser.dto.ts
+++ backend/src/modules/users/dto/updateUser.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { NotIncludeNumberAndSpecialCharacter } from 'src/pipes/custom-validator';
 export class UpdateUserDTO {
   @IsOptional()
@@ -6,5 +6,6 @@ export class UpdateUserDTO {
     message: 'User Name should not include number and special character',
   })
   @IsString({ message: 'User Name should be string' })
+  @MaxLength(30, { message: 'Full Name must < 30 Characters' })
   full_name?: string;
 }
